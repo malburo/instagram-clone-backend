d05a7bde260678add8a6136775460a7ff89a4888
malburo <tongquocbao0605@gmail.com>
2020-06-18 23:58:48 +0700

    base backend

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected');
+});
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+app.use((err, req, res, next) => {
+  res.status(400).json(err);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
