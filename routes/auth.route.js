c6184ef5fbe5663b1874ca4b71a74e405d3d1852
malburo <tongquocbao0605@gmail.com>
2020-06-21 22:17:56 +0700

    route auth

---
+++ routes/auth.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var router = express.Router();
+
+const controller = require('../controllers/auth.controller');
+const validateLogin = require('../middlewares/validations/validateLogin');
+const validateRegister = require('../middlewares/validations/validateRegister');
+
+router.post('/login', validateLogin, controller.login);
+router.post('/register', validateRegister, controller.register);
+router.post('/reset', controller.reset);
+module.exports = router;
