d20745df47f64a6d5d439dc27894886f20c32677
malburo <tongquocbao0605@gmail.com>
2020-08-18 01:16:06 +0700

    create configs folder & move database connect from server.js to configs

---
+++ src/configs/database/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    });
+    console.log('Mongoose is connected!!!');
+  } catch (error) {
+    console.log('Connect failure!!!');
+  }
+}
+
+module.exports = { connect };
