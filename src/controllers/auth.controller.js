b49eeb5426799004f69c094f8b155ebb74f80e99
malburo <tongquocbao0605@gmail.com>
2020-08-18 01:25:05 +0700

    move controllers folder from api to src

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,82 @@
+const User = require('../src/models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.checkToken = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    delete user._doc.password;
+    res.status(201).json({
+      user,
+      isValid: true,
+    });
+  } catch (err) {
+    return next({
+      status: 400,
+      message: err.message,
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return next({
+        status: 401,
+        username: 'Username không tồn tại',
+      });
+    }
+    const checkPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!checkPassword) {
+      return next({
+        status: 401,
+        password: 'Password không đúng',
+      });
+    }
+    delete user._doc.password;
+    const accessToken = jwt.sign(user._doc, process.env.SECRET);
+    res.json({ user, accessToken });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const saltRounds = 10;
+    const { fullname, username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    let newUser = await User.create({
+      fullname,
+      username,
+      email,
+      password: hashedPassword,
+    });
+    delete newUser._doc.password;
+    const accessToken = jwt.sign(newUser._doc, process.env.SECRET);
+    return res.status(201).json({
+      user: newUser,
+      accessToken,
+    });
+  } catch (err) {
+    return next({ status: 400, message: err.message });
+  }
+};
+
+exports.reset = async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({
+        message: 'Email không tồn tại',
+      });
+    }
+    res.json({ message: 'Đã gửi email' });
+  } catch (err) {
+    res.status(400).json(`Error: ${err}`);
+  }
+};
