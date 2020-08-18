02c6330cf8df35debf36a1bc15d6465fcefbbdf5
malburo <tongquocbao0605@gmail.com>
2020-08-19 01:16:18 +0700

    combine like and comment to post controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,12 +1,8 @@
 const Post = require('../models/post.model');
-var fs = require('fs');
-let cloudinary = require('cloudinary').v2;
+const Like = require('../models/like.model');
+const Comment = require('../models/comment.model');
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
+const fs = require('fs');
 
 exports.get = async (req, res, next) => {
   try {
@@ -49,6 +45,59 @@ exports.create = async (req, res, next) => {
     } else {
       next({ status: 400, message: 'upload ko thanh cong' });
     }
+  } catch (err) {
+    return next({ status: 400, message: err.message });
+  }
+};
+
+exports.like = async (req, res, next) => {
+  try {
+    const { postId } = req.body;
+    const userId = req.user._id;
+    const like = await Like.create({
+      userId,
+      postId,
+      isLiked: true,
+    });
+    return res.status(201).json({
+      like,
+    });
+  } catch (err) {
+    return next({ status: 400, message: err.message });
+  }
+};
+
+exports.unlike = async (req, res, next) => {
+  try {
+    const { postId } = req.body;
+    const userId = req.user._id;
+    await Like.findOneAndDelete({
+      userId,
+      postId,
+    });
+    return res.status(201).json({
+      unlike: {
+        userId,
+        postId,
+      },
+    });
+  } catch (err) {
+    return next({ status: 400, message: err.message });
+  }
+};
+
+exports.comment = async (req, res, next) => {
+  try {
+    const { comment, postId } = req.body;
+    let newComment = await Comment.create({
+      userId: req.user._id,
+      postId,
+      content: comment,
+    });
+    newComment._doc.userId = req.user;
+    return res.status(201).json({
+      data: newComment,
+    });
   } catch (err) {
     return next({ status: 400, message: err.message });
   }
