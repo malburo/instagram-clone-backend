64b52197c04096a29c458a9f4710cf5d863a5fa7
malburo <tongquocbao0605@gmail.com>
2020-08-21 22:18:44 +0700

    change useFindAndModify = false

--- src/configs/database/index.js
+++ src/configs/database/index.js
@@ -6,7 +6,7 @@ async function connect() {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: true,
+      useFindAndModify: false,
     });
     console.log('Mongoose is connected!!!');
   } catch (error) {
