81f0d24036c9d89f16486662d2842e347c7a47fc
malburo <tongquocbao0605@gmail.com>
2020-09-04 17:55:11 +0700

    change configs folder

---
+++ src/configs/cloudinary.config.js
@@ -0,0 +1,19 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+exports.uploads = async (file, folder) => {
+  try {
+    const result = await cloudinary.uploader.upload(file, { folder });
+    return {
+      url: result.secure_url,
+      public_id: result.public_id,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
